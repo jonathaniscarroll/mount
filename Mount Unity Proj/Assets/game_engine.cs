17e90f868be0cd99ccefe2f379d54bdd77839a3c
Sam Roberts <samwroberts94@gmail.com>
2016-08-10 12:50:11 -0400

    added empty game running object with timer  for debug purposes

---
+++ Mount Unity Proj/Assets/game_engine.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class game_engine : MonoBehaviour {
+	int timer = 0;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		timer += 1;
+		Debug.Log(timer);
+	}
+}
