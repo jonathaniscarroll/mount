50e2882b19fc1bc51ba2fd7b2957bccec35b5b49
Jonathan Carroll <jonathan@toughguymountain.com>
2016-08-10 15:16:11 -0400

    moving object generation to new script, objectQuantity

---
+++ Mount Unity Proj/Assets/_scripts/objectQuantity.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class objectQuantity : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void generateObject(){
+		objectProb = Random.Range (0f, 100f);
+		if (objectProb <= posts) {
+			Instantiate(cubicleObject, new Vector3(Random.Range (-5f, 5f), 10, Random.Range (-5f, 5f)), Quaternion.identity);
+		}
+}
