c1a25f643edaac098b25a7d094b5182f3a880a52
Jonathan Carroll <jonathan@toughguymountain.com>
2016-08-10 13:59:30 -0400

    starting to create scripting for object generation

---
+++ Mount Unity Proj/Assets/_scripts/cubicleObjectGeneration.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cubicleObjectGeneration : MonoBehaviour {
+
+	game_engine game_engine; 
+
+	// Use this for initialization
+	void Start () {
+		game_engine = GetComponent<
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
