38f6ade031e64f17aa20e2f3b7a7309201c99824
Sam Roberts <samwroberts94@gmail.com>
2016-08-10 13:57:42 -0400

    text and post updates

--- Mount Unity Proj/Assets/_scripts/game_engine.cs
+++ Mount Unity Proj/Assets/_scripts/game_engine.cs
@@ -1,12 +1,17 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class game_engine : MonoBehaviour {
-	public int likes = 0;
-	public int posts = 0;
+	private int likes;
+	private int posts;
+	public Text likeText;
+	public Text postText;
 	// Use this for initialization
 	void Start () {
-
+		likes = 0;
+		posts = 0;
+		setText();
 	}
 
 	// Update is called once per frame
@@ -14,12 +19,17 @@ public class game_engine : MonoBehaviour {
 
 		if ( Input.GetKeyDown ( KeyCode.P )){
 			posts += 1;
-			Debug.Log(posts);
+			setText();
 		}
 
 		if ( Input.GetKeyDown ( KeyCode.L )){
 			likes += 1;
-			Debug.Log(likes);
+			setText();
 		}
+	}
+
+	void setText(){
+		likeText.text = "likes = " + likes.ToString();
+		postText.text = "posts = " + posts.ToString();
 	}
 }
