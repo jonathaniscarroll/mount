e57d2fb6890cbddc19ecbd118ab37122181ae684
Sam Roberts <samwroberts94@gmail.com>
2016-08-12 14:19:57 -0400

    added removal of the bad blue boxes when better gold boxes are available

--- Mount Unity Proj/Assets/_scripts/itemLvDetect.cs
+++ Mount Unity Proj/Assets/_scripts/itemLvDetect.cs
@@ -7,11 +7,16 @@ public class itemLvDetect : MonoBehaviour {
 	//public itemLvVars loaditemLevel;
 
 	public itemLvVars itemLevelRead;
-	public int itemLevelDectected;
+
+	public int topChairLevel;
+	public Rigidbody rigid;
+	private float randomX;
+	private float randomY;
+	private float randomZ;
 
 	// Use this for initialization
 	void Start () {
-		itemLevelDectected = 0;
+		topChairLevel = 0;
 	}
 
 	// Update is called once per frame
@@ -19,10 +24,26 @@ public class itemLvDetect : MonoBehaviour {
 	}
 
 	void OnTriggerEnter (Collider col) {
-		Debug.Log(true);
+		//Debug.Log(true);
 		itemLevelRead = col.GetComponent<itemLvVars>();
 		if(itemLevelRead != null){
-		Debug.Log(itemLevelRead.itemLevel);
+		//Debug.Log(itemLevelRead.itemLevel);
+
+		if(col.tag == "chair"){
+			if(itemLevelRead.itemLevel > topChairLevel){
+				topChairLevel = itemLevelRead.itemLevel;
+				Debug.Log("NEW TOP LEVEL" + topChairLevel);
+			}
+			if(topChairLevel > itemLevelRead.itemLevel){
+					randomX = (Random.value * 100f)*10;
+					randomY = (Random.value * 100f)*10;
+					randomZ = (Random.value * 100f)*10;
+
+					rigid = col.GetComponent<Rigidbody>();
+					rigid.AddForce(randomX, randomY, randomZ, ForceMode.Force);
+					Debug.Log("PUNCH");
+			}
 		}
+	}
 	}
 }
