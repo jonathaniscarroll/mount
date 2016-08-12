8331dce2f7f1124eb484cd37e523726aa7206e52
Sam Roberts <samwroberts94@gmail.com>
2016-08-12 13:37:39 -0400

    added item level, detection of item level, new hotbox around player for finding item level

---
+++ Mount Unity Proj/Assets/_scripts/itemLvDetect.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class itemLvDetect : MonoBehaviour {
+
+	//public int readItemLevel;
+	//public itemLvVars loaditemLevel;
+
+	public itemLvVars itemLevelRead;
+	public int itemLevelDectected;
+
+	// Use this for initialization
+	void Start () {
+		itemLevelDectected = 0;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnTriggerEnter (Collider col) {
+		Debug.Log(true);
+		itemLevelRead = col.GetComponent<itemLvVars>();
+		if(itemLevelRead != null){
+		Debug.Log(itemLevelRead.itemLevel);
+		}
+	}
+}
