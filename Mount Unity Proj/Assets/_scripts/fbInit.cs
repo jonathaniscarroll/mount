f312c25d447f85562a319d2444daac549543061b
Jonathan Carroll <jonathan@toughguymountain.com>
2016-08-13 10:42:59 -0400

    made a couple changes to the fbInit, working towards reading people's posts. Posts are retrieved as dictionaries within dictionaries: saved as a dictionary of posts, each post being a dictionary of the information about the post, its ID etc

--- Mount Unity Proj/Assets/_scripts/fbInit.cs
+++ Mount Unity Proj/Assets/_scripts/fbInit.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class fbInit : MonoBehaviour {
 
 	List<string> perm;
+	private Dictionary<string,object> FBUserDetails;
+	private string fbID;
 
 	// Awake function from Unity's MonoBehavior
 	void Awake ()
@@ -23,16 +25,41 @@ public class fbInit : MonoBehaviour {
 		FB.LogInWithReadPermissions(perm, AuthCallback);
 	}
 
+	void retrievePosts(){
+		FB.API ("/me/posts", HttpMethod.GET,postsCallback,new Dictionary<string,string>(){});
+	}
+
 	private void AuthCallback (ILoginResult result) {
 		if (FB.IsLoggedIn) {
+			// AccessToken class will have session details
+			var aToken = Facebook.Unity.AccessToken.CurrentAccessToken;
+			// Print current access token's User ID
+			Debug.Log(aToken.UserId);
 			// Print current access token's granted permissions
-			foreach (string str in perm) {
-				Debug.Log(str);
-			}
+//			foreach (string perm in Facebook.Unity.AccessToken.CurrentAccessToken.Permissions) {
+//				Debug.Log(perm);
+//			}
+			retrievePosts ();
 		} else {
 			Debug.Log("User cancelled login");
 		}
 
+	}
+
+	private void postsCallback(IGraphResult result){
+		Debug.Log (result.RawResult);
+
+		FBUserDetails = (Dictionary<string,object>)result.ResultDictionary;
+
+		//Debug.Log ("POSTS ITS POSSST: " + FBUserDetails["posts"]);
+
+		foreach(KeyValuePair<string,object> keyValue in FBUserDetails)
+		{
+			string key = keyValue.Key;
+			object value = keyValue.Value;
+
+			Debug.Log ("KEY: " + key + " VALUE: " + value);
+		}
 	}
 
 }
