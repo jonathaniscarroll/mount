173cfa8278b86bd8b53c18be0a6ccf0767461506
Sam Roberts <samwroberts94@gmail.com>
2016-08-15 16:43:41 -0400

    uh late update sorry jonathan this one has more info and developments in the translation system

--- Mount Unity Proj/Assets/_scripts/interaction.cs
+++ Mount Unity Proj/Assets/_scripts/interaction.cs
@@ -41,12 +41,25 @@ public class interaction : MonoBehaviour {
 		if(col.tag == "pencil"){
 			//get collider's game object, put that into pickuphold
 			PickUpandHold (col.gameObject);
-			Debug.Log(col.gameObject);
+			//Debug.Log(col.gameObject);
 		}
 	}
 
 	void PickUpandHold (GameObject item) {
+		Vector3 internPosiHold = new Vector3 (intern.transform.position.x + 1, 2.0f, intern.transform.position.z+ 1);
+
+		//item.transform.Translate(internPosiHold * (Time.deltaTime * 0.1f), Space.Self);
+//		private float journey
+//		journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
+//		float distCovered = (Time.time - startTime) * speed;
+//		float fracJourney = distCovered / journeyLength;
+
+//		item.transform.position = Vector3.MoveTowards(item.transform.position, internPosiHold, Time.deltaTime * 0.1f);
 		item.transform.SetParent(intern.transform, true);
-		item.rigidbody.Gravity = false;
+		item.transform.position = Vector3.Lerp(item.transform.position, internPosiHold, );
+		item.GetComponent<Rigidbody>().useGravity = false;
+
+		item.GetComponent<Rigidbody>().isKinematic = true;
+
 	}
 }
