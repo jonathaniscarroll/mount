4ddc4d2f3d20ee486f32c88d46dd4e3082f04dcf
Jonathan Carroll <jonathan@toughguymountain.com>
2016-08-17 17:20:03 -0400

    Walkway to secondary intern platforms now instantiates at open

--- Mount Unity Proj/Assets/_scripts/cubicleGeneration.cs
+++ Mount Unity Proj/Assets/_scripts/cubicleGeneration.cs
@@ -1,15 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic; //this is necessary for using lists
 
 public class cubicleGeneration : MonoBehaviour {
 
+	public game_engine GameEngine;
+	public List<GameObject> cubicles = new List<GameObject>();
+
+	private GameObject newestCubicle;
+	private List<Vector3> directions = new List<Vector3> ();
+	private Vector3 cubiclePos;
+	public GameObject pathway;
+
 	// Use this for initialization
 	void Start () {
-	
+		GameEngine = gameObject.GetComponent<game_engine>();
+		newestCubicle = GameObject.FindGameObjectWithTag ("cubicle");
+		cubiclePos = newestCubicle.transform.position;
+		newCubicle();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void newCubicle(){
+		directions.Add (cubiclePos + new Vector3 (6, 0, 0));
+		directions.Add (cubiclePos + new Vector3 (-6, 0, 0));
+		directions.Add (cubiclePos + new Vector3 (0, 0, 6));
+		directions.Add (cubiclePos + new Vector3 (0, 0, -6));
+		Instantiate (pathway, directions [Random.Range (0,4)], Quaternion.identity);
 	}
 }
