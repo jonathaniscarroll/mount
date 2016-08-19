f06998f7a5e634db234675cf0e7174ea4d2899af
Jonathan Carroll <jonathan@toughguymountain.com>
2016-08-19 14:28:46 -0400

    Working on an object spawn weight dictionary

--- Mount Unity Proj/Assets/_scripts/objectDictionary.cs
+++ Mount Unity Proj/Assets/_scripts/objectDictionary.cs
@@ -6,7 +6,11 @@ public class objectDictionary : MonoBehaviour {
 
 	public List<GameObject> cubicleObjects = new List<GameObject>();
 
-	public Dictionary<int,GameObject> cubobjDictionary; 
+	public Dictionary<int,GameObject> cubobjDictionary;
+	public Dictionary<int,GameObject> weightDictionary;
+
+	private GameObject[] testWeight;
+	private string objTag;
 
 	// Use this for initialization
 	void Start () {
@@ -16,9 +20,13 @@ public class objectDictionary : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if ( Input.GetKeyDown ( KeyCode.Space )){
+			weightObjects ();
+		}
 	}
 
+
+	// each object has a base like level required to spawn that item. This is called in the object quantity script
 	void makeObjectDictionary(){
 		int i = 1;
 		int o;
@@ -29,6 +37,25 @@ public class objectDictionary : MonoBehaviour {
 			cubobjDictionary.Add (o,cubobj);
 			//Debug.Log (cubobjDictionary[o]);
 			i++;
+		}
+	}
+	//weight object generation based on which objects already exist
+	void weightObjects(){
+
+		int i = 0;
+
+		foreach (GameObject cubobj in cubicleObjects) {
+
+			objTag = cubobj.tag;
+			//find how many of each game object is in the scene
+			testWeight = GameObject.FindGameObjectsWithTag (objTag);
+
+			Debug.Log(cubobj + ": " + testWeight.Length);
+
+//			foreach (GameObject objectWeight in testWeight) {
+//				weightDictionary.ADD ();
+//				i++;
+//			}
 		}
 	}
 }
