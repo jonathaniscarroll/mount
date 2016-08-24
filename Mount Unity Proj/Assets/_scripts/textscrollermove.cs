08ad4e252ac2b2780b47e250086a5bdd842261ae
Sam Roberts <samwroberts94@gmail.com>
2016-08-24 15:16:45 -0400

    press t to scroll unmodifiable text across the screen, picking up paper and a pencil at the same time causes text to a pear on the pencil

---
+++ Mount Unity Proj/Assets/_scripts/textscrollermove.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class textscrollermove : MonoBehaviour {
+
+
+	public GameObject endtext;
+	public int speed;
+
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		rb.AddForce(speed,0,0,ForceMode.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//rb.AddForce(speed,0,0,ForceMode.Force);
+		if(transform.position.x <= endtext.transform.position.x){
+			Destroy(gameObject);
+		}
+	}
+}
